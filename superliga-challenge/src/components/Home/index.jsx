95ce8d42183e21922db76a6e24f6be340f8459ba
MonagasVegas <35318244+MonagasVegas@users.noreply.github.com>
2024-06-14 20:18:06 -0300

    feat: add

--- superliga-challenge/src/components/Home/index.jsx
+++ superliga-challenge/src/components/Home/index.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Header from "../Header";
+import Container from "../container";
 
 const Home = () => {
   return (
-    <div className="">   
-   <div>
-    <Header />
-   </div>
-   
-   
+    <div className="flex flex-col gap-10">
+      <div>
+        <Header />
+      </div>
+
+      <div >
+        <Container />
+      </div>
     </div>
   );
 };
