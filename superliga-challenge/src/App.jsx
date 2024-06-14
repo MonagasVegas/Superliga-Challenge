4059e713887f814bfea819990affafe8baf154de
MonagasVegas <35318244+MonagasVegas@users.noreply.github.com>
2024-06-14 12:28:02 -0300

    feat: add

--- superliga-challenge/src/App.jsx
+++ superliga-challenge/src/App.jsx
@@ -1,11 +1,20 @@
 import { Routes, Route, HashRouter } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <HashRouter>
         <Routes>
-          <Route path="*" element={<h1 className="text-2xl font-bold flex justify-center py-20">Not Found</h1>} />
+          <Route path="/" element={<Home />} />
+          <Route
+            path="*"
+            element={
+              <h1 className="text-2xl font-bold flex justify-center py-20">
+                Not Found
+              </h1>
+            }
+          />
         </Routes>
       </HashRouter>
     </>
